373cd565643dd17d4fa441506af0c617ca4f02d7
Basic Lady Programmer <84853907+basicladyprogrammer@users.noreply.github.com>
2021-11-14 18:18:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let header = document.querySelector('.header-2');
+
+
+menu.addEventListener('click', () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+});
+
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+if(window.scrolly > 150){
+  header.classList.add('active');
+}else {
+  header.classList.remove('active');
+}
+}
+<script>
+function userclick(){
+  alert(product.img);
+}
+</script>
